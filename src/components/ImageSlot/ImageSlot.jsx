7c1da94cfff70dc54118812cdf42c915677d25e2
Lambtsa <42414095+Lambtsa@users.noreply.github.com>
2021-03-31 16:58:51 +0200

    adding images with cloudinary done

---
+++ src/components/ImageSlot/ImageSlot.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimesCircle as deleteIcon } from '@fortawesome/free-solid-svg-icons';
+import './ImageSlot.scss';
+
+const ImageSlot = ({ itemTitle = '', setItemImages }) => {
+  const [imageUrl, setImageUrl] = useState('');
+  const [error, setError] = useState(false);
+
+  const handleFileChange = e => {
+    const url = 'https://api.cloudinary.com/v1_1/dk9je5ll6/upload';
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append('upload_preset', 'trading-app');
+    formData.append('file', file);
+    fetch(url, {
+      method: 'POST',
+      body: formData,
+    })
+      .then(response => response.json())
+      .then(data => {
+        setImageUrl(data.secure_url);
+        setItemImages(prevState => [...prevState, data.secure_url]);
+      })
+      .catch(() => setError(true));
+  };
+
+  const handleDeleteClick = () => {
+    setImageUrl('');
+  };
+
+  return (
+    <>
+      {imageUrl !== '' && (
+        <div className="ImageSlot__container">
+          <button className="ImageSlot__btn" type="button" onClick={handleDeleteClick}>
+            <FontAwesomeIcon icon={deleteIcon} className="ImageSlot__delete" />
+          </button>
+          <img className="ImageSlot__img" src={imageUrl} alt={itemTitle} />
+        </div>
+      )}
+      {imageUrl === '' && (
+        <label className="form__label" htmlFor="image-1">
+          Item Images:
+          <input className="form__input" id="image-1" type="file" onChange={handleFileChange} accept="image/png, image/jpeg" />
+        </label>
+      )}
+      {error && <p className="error-message">There was an error in uploading your image</p>}
+    </>
+  );
+};
+
+export default ImageSlot;
--- src/views/NewItem.jsx
+++ src/views/NewItem.jsx
@@ -2,17 +2,18 @@ import React, { useState } from 'react';
 import { useOktaAuth } from '@okta/okta-react';
 import './NewItem.scss';
 import { postItemToAPI } from '../modules/api-service';
+import ImageSlot from '../components/ImageSlot/ImageSlot';
 
 const NewItem = () => {
   const { authState } = useOktaAuth();
   const [itemTitle, setItemTitle] = useState('');
   const [itemDescription, setItemDescription] = useState('');
-  const [itemImages, setItemImages] = useState('');
+  const [itemImages, setItemImages] = useState([]);
   const [itemCategory, setItemCategory] = useState('');
+  const [message, setMessage] = useState('');
 
   const onTitleChange = e => setItemTitle(e.target.value);
   const onDescriptionChange = e => setItemDescription(e.target.value);
-  const onImageChange = e => setItemImages(e.target.value);
   const onCategoryChange = e => setItemCategory(e.target.value);
 
   const handleFormSubmit = async e => {
@@ -23,7 +24,7 @@ const NewItem = () => {
       item: {
         itemTitle,
         itemDescription,
-        itemImages: [itemImages],
+        itemImages,
         itemCategory,
       },
     };
@@ -31,6 +32,11 @@ const NewItem = () => {
       const response = await postItemToAPI(newItem, accessToken);
       const data = await response.json();
       console.log(data);
+      setItemTitle('');
+      setItemDescription('');
+      setItemImages([]);
+      setItemCategory('');
+      setMessage('Your Item has been added');
     } catch (err) {
       /* better error handling */
       console.log(err);
@@ -38,30 +44,30 @@ const NewItem = () => {
   };
 
   return (
-    <form className="form" onSubmit={handleFormSubmit}>
-      <label className="form__label" htmlFor="title">
-        Item Title:
-        <input className="form__input" id="title" type="text" value={itemTitle} onChange={onTitleChange} placeholder="Enter title" required />
-      </label>
-      <label className="form__label" htmlFor="description">
-        Item Description:
-        <input className="form__input" id="description" type="text" value={itemDescription} onChange={onDescriptionChange} placeholder="Enter description" required />
-      </label>
-      <label className="form__label" htmlFor="images">
-        Item Images:
-        <input className="form__input" id="images" type="text" value={itemImages} onChange={onImageChange} placeholder="Enter image url" required />
-      </label>
-      <label className="form__label" htmlFor="category">
-        Item Category:
-        <select className="form__input" id="category" value={itemCategory} onChange={onCategoryChange} required>
-          <option value="">Please select category</option>
-          <option value="furniture">Furniture</option>
-          <option value="clothes">Clothes</option>
-          <option value="books">Books</option>
-        </select>
-      </label>
-      <button className="form__btn" type="submit">Add item</button>
-    </form>
+    <>
+      <form className="form" onSubmit={handleFormSubmit}>
+        <label className="form__label" htmlFor="title">
+          Item Title:
+          <input className="form__input" id="title" type="text" value={itemTitle} onChange={onTitleChange} placeholder="Enter title" required />
+        </label>
+        <label className="form__label" htmlFor="description">
+          Item Description:
+          <input className="form__input" id="description" type="text" value={itemDescription} onChange={onDescriptionChange} placeholder="Enter description" required />
+        </label>
+        <ImageSlot setItemImages={setItemImages} itemTitle={itemTitle} />
+        <label className="form__label" htmlFor="category">
+          Item Category:
+          <select className="form__input" id="category" value={itemCategory} onChange={onCategoryChange} required>
+            <option value="">Please select category</option>
+            <option value="furniture">Furniture</option>
+            <option value="clothes">Clothes</option>
+            <option value="books">Books</option>
+          </select>
+        </label>
+        <button className="form__btn" type="submit">Add item</button>
+        {message !== '' && <p className="snack-message">{message}</p>}
+      </form>
+    </>
   );
 };
 
