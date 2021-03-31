5c8ae9e4c45cf9782486c8d1c94480510d2492c9
Lambtsa <42414095+Lambtsa@users.noreply.github.com>
2021-03-31 15:27:34 +0200

    my account name

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useOktaAuth } from '@okta/okta-react';
 import './Header.scss';
 import { Link, useHistory } from 'react-router-dom';
 
+/* eslint-disable */
 const Header = () => {
   const { authState, oktaAuth } = useOktaAuth();
+  const [displayName, setDisplayName] = useState('My account');
   const history = useHistory();
 
   const button = authState.isAuthenticated
     ? <button type="button" onClick={() => oktaAuth.signOut()}>Logout</button>
     : <button type="button" onClick={() => history.push('/login')}>Login</button>;
+
+  useEffect(() => {
+    if (authState.isAuthenticated) {
+      oktaAuth.token.getUserInfo().then(info => {
+        setDisplayName(info.nickname);
+      });
+    } 
+  }, [authState, oktaAuth]);
 
   return (
     <header className="header">
@@ -19,7 +29,7 @@ const Header = () => {
           <Link className="header__link" to="/new-item">New Item</Link>
           <Link className="header__link" to="/trading">Trading</Link>
           <Link className="header__link" to="/likes">Likes</Link>
-          {authState.isAuthenticated && <Link className="header__link" to="/my-account">My account</Link>}
+          {authState.isAuthenticated && <Link className="header__link" to="/my-account">{displayName}</Link>}
           {!authState.isAuthenticated && <Link className="header__link" to="/signup">SignUp</Link>}
           {button}
         </nav>
