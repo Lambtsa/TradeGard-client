0ce55645004f25ea7885e2be9a500367665a72c3
Oscar Lindqvist <olindqv@gmail.com>
2021-04-06 15:06:34 +0200

    trading page refactor

--- src/components/MatchCard/MatchCard.jsx
+++ src/components/MatchCard/MatchCard.jsx
@@ -1,23 +1,48 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope as envelopeIcon } from '@fortawesome/free-solid-svg-icons';
+import {
+  faEnvelope as envelopeIcon,
+  faAngleDown as angleIconDown,
+  faAngleUp as angleIconUp,
+} from '@fortawesome/free-solid-svg-icons';
 import ContactModal from '../ContactModal/ContactModal';
 
 const MatchCard = ({ matchDetails }) => {
   const [showModal, setShowModal] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
 
   return (
     <>
       <li className="MatchCard">
-        <div className="MatchCard__userCircle">{matchDetails.userDisplayName[0].toUpperCase()}</div>
-        <div className="MatchCard__user">
-          <h3 className="MatchCard__header">{matchDetails.userDisplayName}</h3>
-          <p className="MatchCard__text">They have liked</p>
-          <p className="MatchCard__text count">{`${matchDetails.userLikeCount} items`}</p>
+        <div className="MatchCard__top">
+          <div className="MatchCard__userCircle">{matchDetails.userDisplayName[0].toUpperCase()}</div>
+          <div className="MatchCard__user">
+            <h3 className="MatchCard__header">{matchDetails.userDisplayName}</h3>
+            <p className="MatchCard__text">They have liked</p>
+            <button type="button" onClick={() => setIsVisible(!isVisible)} className="MatchCard__text count">
+              <>
+                {`${matchDetails.userLikeCount} of your items`}
+                <FontAwesomeIcon className="caret" icon={isVisible ? angleIconDown : angleIconUp} />
+              </>
+            </button>
+          </div>
+          <button type="button" onClick={() => setShowModal(true)}>
+            <FontAwesomeIcon icon={envelopeIcon} className="MatchCard__envelopeIcon" />
+          </button>
         </div>
-        <button type="button" onClick={() => setShowModal(true)}>
-          <FontAwesomeIcon icon={envelopeIcon} className="MatchCard__envelopeIcon" />
-        </button>
+        {isVisible && (
+          <div className="MatchCard__bottom">
+            <div className="MatchCard__list">
+              <h4>Their likes</h4>
+              {matchDetails.theirLikes.map(like => <Link className="MatchCard__link" key={like.id} to={`/items/${like.id}`}>{like.itemTitle}</Link>)}
+            </div>
+            <div className="MatchCard__list">
+              <h4>Your likes</h4>
+              {matchDetails.yourLikes.map(like => <Link className="MatchCard__link" key={like.id} to={`/items/${like.id}`}>{like.itemTitle}</Link>)}
+            </div>
+          </div>
+        )}
       </li>
       {showModal && <ContactModal setShowModal={setShowModal} ownerDetails={matchDetails} />}
     </>
